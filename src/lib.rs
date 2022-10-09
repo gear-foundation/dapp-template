6e2593fe09e2d0c6c50482c2fea33f896825b7f8
Shamil <66209982+shamilsan@users.noreply.github.com>
2022-10-09 12:22:04 +0300

    Add metadata (#7)

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,61 @@
 #![no_std]
 
-use gstd::{debug, msg, prelude::*};
+use gstd::{debug, metadata, msg, prelude::*};
 
-#[no_mangle]
-unsafe extern "C" fn handle() {
-    debug!("handle()");
-    let payload = String::from_utf8(msg::load_bytes()).expect("Invalid handle message");
+#[derive(Decode, Encode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+struct Payload {
+    question: String,
+    answer: u8,
+}
 
-    if payload == "Hello" {
-        msg::reply(b"World", 0).unwrap();
-    }
+metadata! {
+    title: "App",
+    handle:
+        input: Payload,
+        output: u8,
 }
 
 #[no_mangle]
-unsafe extern "C" fn init() {
+extern "C" fn init() {
     let payload = String::from_utf8(msg::load_bytes()).expect("Invalid init message");
     debug!("init(): {}", payload);
 }
 
+#[no_mangle]
+extern "C" fn handle() {
+    debug!("handle()");
+    let payload: Payload = msg::load().expect("Unable to decode payload");
+    if payload.question == "life-universe-everything" {
+        msg::reply(payload.answer, 0).unwrap();
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    extern crate std;
-
+    use super::Payload;
+    use gstd::ToString;
     use gtest::{Log, Program, System};
 
     #[test]
-    fn it_works() {
+    fn question_answer() {
         let system = System::new();
         system.init_logger();
 
         let program = Program::current(&system);
 
-        let res = program.send_bytes(42, "Let's start");
+        let res = program.send_bytes(12, "Let's start");
         assert!(res.log().is_empty());
 
-        let res = program.send_bytes(42, "Hello");
-        let log = Log::builder().source(1).dest(42).payload_bytes("World");
+        let res = program.send(
+            12,
+            Payload {
+                question: "life-universe-everything".to_string(),
+                answer: 42,
+            },
+        );
+        let log = Log::builder().source(1).dest(12).payload(42_u8);
         assert!(res.contains(&log));
     }
 }
