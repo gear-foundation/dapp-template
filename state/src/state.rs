442bdc27e64aa86f38ec805e54a18ccd43918f5f
Shamil <66209982+shamilsan@users.noreply.github.com>
2023-02-28 01:05:05 +0300

    Update deps

--- state/src/state.rs
+++ state/src/state.rs
@@ -3,14 +3,14 @@ use gmeta::{metawasm, Metadata};
 use gstd::{prelude::*, ActorId};
 
 #[metawasm]
-pub trait Metawasm {
-    type State = <ContractMetadata as Metadata>::State;
+pub mod metafns {
+    pub type State = <ContractMetadata as Metadata>::State;
 
-    fn pingers(state: Self::State) -> Vec<ActorId> {
+    pub fn pingers(state: State) -> Vec<ActorId> {
         state.pingers()
     }
 
-    fn ping_count(actor: ActorId, state: Self::State) -> u128 {
+    pub fn ping_count(state: State, actor: ActorId) -> u128 {
         state.ping_count(actor)
     }
 }
